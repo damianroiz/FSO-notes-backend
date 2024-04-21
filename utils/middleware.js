8570e44500abf269f426eb877ab085fb2b035d1a
Damian Roiz <damianx989@gmail.com>
2024-04-20 21:18:32 -0400

    token authentication

--- utils/middleware.js
+++ utils/middleware.js
@@ -17,9 +17,12 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
   }
+
   if (error.name === "ValidationError") {
     return response.status(400).json({ error: error.message });
-  } else if (
+  }
+
+  if (
     error.name === "MongoServerError" &&
     error.message.includes("E11000 duplicate key error")
   ) {
@@ -27,6 +30,17 @@ const errorHandler = (error, request, response, next) => {
       .status(400)
       .json({ error: "expected `username` to be unique" });
   }
+
+  if (error.name === "JsonWebTokenError") {
+    return response.status(401).json({ error: "token invalid" });
+  }
+
+  if (error.name === "TokenExpiredError") {
+    return response.status(401).json({
+      error: "token expired",
+    });
+  }
+
   next(error);
 };
 
